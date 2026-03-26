c88f85a7d03a65b4b4d21df139a092be216f0cc2
Turker TUNALI <turker.tunali@gmail.com>
2026-03-26 08:04:53 +0000

    Kargo etiketi yazdirma kontrolu eklendi.

---
+++ yurtici_kargo_erpnext/yurtici_kargo_erpnext/doctype/yurtici_kargo_ayarlari/test_yurtici_kargo_ayarlari.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Logedosoft Business Solutions and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestYurticiKargoAyarlari(FrappeTestCase):
+	pass
---
+++ yurtici_kargo_erpnext/yurtici_kargo_erpnext/doctype/yurtici_kargo_ayarlari/yurtici_kargo_ayarlari.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Logedosoft Business Solutions and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Yurtici Kargo Ayarlari", {
+// 	refresh(frm) {
+
+// 	},
+// });
